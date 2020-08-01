bcb9b290a32f4353a44b213af4117a00931812f1
Anurag Shekhawat <anuragshekhawat1995@gmail.com>
2020-08-01 12:37:41 +0530

    Blog FIrst commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = (props) =>{
+    return(
+        // <Header>
+        //     <nav>
+        //         <a href = "#">Home</a>
+        //         <a href = "#">AboutUs</a>
+        //         <a href = "#">COntactUs</a>
+        //     </nav>
+        // </Header>
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Header
